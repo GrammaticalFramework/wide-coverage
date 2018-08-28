ac316a580809dac8146350e1b68767c4217f6315
Krasimir Angelov <kr.angelov@gmail.com>
2018-08-28 14:18:50 +0200

    added documentation

--- translator/classify.hs
+++ translator/classify.hs
@@ -12,6 +12,44 @@ import System.Environment
 import Translit
 import Control.Monad(forM_)
 
+{- HOW TO USE IT
+   ~~~~~~~~~~~~~
+   
+   You need data from the Open Multilingual WordNet for 
+   English, Finnish and the language that you are interested in.
+   The data files for each language should be stored in
+   `data/wn-data-<language code>.tab´. You also need to store the file
+   `fiwn-transls.tsv` from the Finnish WordNet in the `data` folder.
+   
+   Now do the following:
+   
+   1. Change the ´train´ variable bellow to ´True´ then run 
+   the training:
+     
+        > runghc classify.hs eng fin
+        
+   This will create a file called ´table.csv´ which you will need
+   in the next step. Everything else is irrelevant unless if you 
+   are interested in Finnish.
+   
+   2. 
+   
+   3. Change the variable ´train´ back to ´False´, then 
+   run the classification. For example:
+   
+        > runghc classify.hs eng por
+        
+   If you are interested in Portuguese. This will create a file
+   called ´predictions.tsv´ which lists a synset and 
+   a pair of eng-por candidate translation from that synset.
+   If the last column in the file is ´True´ then
+   the algorithm thinks that this is a good candidate.
+   
+   If you want to bootstrap a translation dictionary from WordNet
+   then select only the pairs for which the status is True.
+   For good quality the output still needs human validation.
+-}
+
 train = False
 useTenFold = False
 
